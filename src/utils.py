ae6640d0e5b89d13782b34177e6fa1bc1e61c1e7
Rahul <rahul.singh.7920000@gmail.com>
2026-08-02 00:20:14 +0530

    Hyperparaneter Training

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,7 @@ import pickle
 import dill
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 def save_object(file_path, obj):
@@ -23,13 +24,22 @@ def save_object(file_path, obj):
 
 
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, para, cv=3)
+
+            gs.fit(X_train, y_train)  # Train model
+
+            model.set_params(**gs.best_params_)  # Set best parameters
             model.fit(X_train, y_train)  # Train model
+
+            # model.fit(X_train, y_train)  # Train model
             y_train_pred = model.predict(X_train)  # Predict training data
             y_test_pred = model.predict(X_test)  # Predict Testing data
             train_model_score = r2_score(y_train, y_train_pred)  # Evaluate model
